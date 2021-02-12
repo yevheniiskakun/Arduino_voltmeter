3e66408965f0e31b6b2bc1315f9ccbb703c1786c
eugeneshark <yevhenii.skakun@gmail.com>
2021-02-12 10:36:22 +0100

    Make code more beautiful and deleted temporary variables

--- Voltmeter/Voltmeter.ino
+++ Voltmeter/Voltmeter.ino
@@ -3,7 +3,7 @@
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 float input_voltage = 0.0;
-float temp=0.0;
+int analog_value;
 
 void setup()
 {
@@ -11,21 +11,21 @@ void setup()
  // Print a message to the LCD.
  lcd.backlight();
  lcd.print("Voltmeter");
+ delay(1000);
 }
 void loop()
 {
-  int analog_value = analogRead(A0);
-   input_voltage = (analog_value * 5.0) / 1024.0; 
+  analog_value = analogRead(A0);
+  input_voltage = (analog_value * 5.0) / 1024.0; 
 
    
    if (input_voltage < 0.1) 
    {
      input_voltage=0.0;
    } 
-    Serial.print("v= ");
-    Serial.println(input_voltage);
+    
     lcd.setCursor(0, 1);
-    lcd.print("Voltage= ");
+    lcd.print("Voltage = ");
     lcd.print(input_voltage);
     delay(300);
 }
